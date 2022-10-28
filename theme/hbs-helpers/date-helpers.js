a51b7c4b13a0f9d387a0a9bc736ef89cf563d978
Baptiste Pesquet <bpesquet@gmail.com>
2022-10-28 17:40:50 +0200

    Translations needed for my resume

--- theme/hbs-helpers/date-helpers.js
+++ theme/hbs-helpers/date-helpers.js
@@ -1,5 +1,8 @@
 const moment = require('moment');
 
+// Change locale to format dates the french way
+moment.locale('fr')
+
 const dateHelpers = {
   MY: date => moment(date.toString(), ['YYYY-MM-DD']).format('MMM YYYY'),
   Y: date => moment(date.toString(), ['YYYY-MM-DD']).format('YYYY'),
